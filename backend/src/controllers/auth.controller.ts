9ae6727802122d334e36eb5765b0f9317943c532
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-03-05 21:14:33 +0100

    [fix] error in auth.controller.ts | Files: backend/src/controllers/auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -19,7 +19,8 @@ export const Login: RequestHandler = async (req: Request, res: Response): Promis
 }
 
 export const Signup: RequestHandler = async (req: Request, res: Response): Promise<void> => {
-    const { username, email, password, name, lastname, visibility, profilePicture, bio, socialLinks } = req.body;
+    const { username, email, password, name, lastname, visibility, profilePicture, bio, socialLinks
+    } = req.body;
 
     if (!username) {
         res.status(422).send({ message: 'username is missing' })
@@ -46,13 +47,13 @@ export const Signup: RequestHandler = async (req: Request, res: Response): Promi
         return
     }
 
-    if (visibility !== null) {
+    if (!visibility) {
         if (visibility !== Visibility.PUBLIC || visibility !== Visibility.PRIVATE || visibility !== Visibility.FRIENDS_ONLY)
             res.status(422).send({ message: `visibility can not be ${visibility} it has to be one of these ${Visibility.PUBLIC}, ${Visibility.PRIVATE}, ${Visibility.FRIENDS_ONLY}` })
         return
     }
 
-    if (socialLinks !== null) {
+    if (!socialLinks) {
         if (typeof socialLinks !== "object" && !Array.isArray(socialLinks)) {
             res.status(422).send({ message: `socialLinks has to be an Array` })
             return
