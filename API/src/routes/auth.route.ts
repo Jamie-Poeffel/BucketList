09f2af8956b74393466055d82cc556a6ac25eb3d
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-03-05 22:07:30 +0100

    [rename]  | Files: API/Dockerfile, API/package-lock.json, API/package.json, API/src/app.ts, API/src/config/app.ts, API/src/config/db.ts, API/src/controllers/auth.controller.ts, API/src/controllers/user.controller.ts, API/src/middlewares/authMiddleware.ts, API/src/models/user.model.ts, API/src/routes/auth.route.ts, API/src/routes/user.route.ts, API/src/services/auth.service.ts, API/src/services/user.service.ts, API/tsconfig.json

--- API/src/routes/auth.route.ts
+++ API/src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { Signup, Login } from './../controllers/auth.controller';
+import { Signup, Login } from '../controllers/auth.controller';
 
 const router = Router();
 
