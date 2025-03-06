6a527518b0d94db13f8a5cfccf497ec836344e81
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-03-06 19:12:10 +0100

    [fix] . | Files: API/src/routes/auth.route.ts

--- API/src/routes/auth.route.ts
+++ API/src/routes/auth.route.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import { Signup, Login } from '../controllers/auth.controller';
-import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/signup', Signup);
 router.post('/login', Login);
-router.use('/profile', authMiddleware)
 
 export default router
