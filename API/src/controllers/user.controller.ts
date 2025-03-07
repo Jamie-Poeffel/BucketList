b9d383d126ce84e32c4d01412cd997b37c7b15df
Jamie Poeffel <168637927+Jamie-Poeffel@users.noreply.github.com>
2025-03-07 16:11:02 +0100

    user.controller.ts aktualisieren

--- API/src/controllers/user.controller.ts
+++ API/src/controllers/user.controller.ts
@@ -1,10 +1,10 @@
-﻿import { Request, Response, RequestHandler } from 'express';
+import { Request, Response, RequestHandler } from 'express';
 import { GetAllUsers, DeleteUser, GetUserCount } from '../services/user.service';
 
 export const getUsers: RequestHandler = async (req: Request, res: Response): Promise<void> => {
     try {
         const users = await GetAllUsers();
-        res.status(200).json(users);  // You modify the response here
+        res.status(200).json(users); 
     } catch (error) {
         console.error('Error fetching users:', error);
         res.status(500).json({ message: 'Failed to fetch users', error: error });
